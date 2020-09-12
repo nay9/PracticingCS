5e402f0d486a23eafbb28d0ff3bc3d61624de56a
nay9 <renenoe94@hotmail.com>
2020-09-11 22:56:34 -0400

    pushing stuff

---
+++ UnderstandingScope/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnderstandingScope
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int j = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                j = i;
+                Console.WriteLine(i);
+            }
+            Console.WriteLine(j);
+            Car myCar = new Car();
+            myCar.DoSomething();
+        }
+    }
+
+    class Car 
+    {
+        public void DoSomething()
+        {
+            Console.WriteLine(helperMethod());
+        }
+
+        private string helperMethod()
+        {
+            return "hello";
+        }
+    }
+
+
+
+}
