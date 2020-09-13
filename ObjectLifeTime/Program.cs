7844f07e51b91bc98476894c62a7dfb03ab1423a
nay9 <renenoe94@hotmail.com>
2020-09-13 13:38:56 -0400

    pushing up code

---
+++ ObjectLifeTime/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectLifeTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+
+            myCar.Make = "oldsmobile";
+            myCar.Model = "cutlass";
+            myCar.Year = 1111;
+            myCar.Color = "blue";
+
+            Car myOtherCar;
+            myOtherCar = myCar;
+
+
+            myOtherCar.Model = "van";
+
+            Car myNewCar = new Car("ford", "fusion", 2012, "blue");
+        }
+    }
+
+    class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+
+
+
+        public Car()
+        {
+
+
+            this.Make = "Nissan";
+        }
+
+        public Car(string make, string model, int year, string color)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+            Color = color;
+        }
+    }
+}
